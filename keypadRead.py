c8e33d7f9a77e6c1870d13ca81236273f4504617
Filip B <spankie1337@users.noreply.github.com>
2017-04-25 14:17:59 +0100

    fixed key2pi and pi2key

--- keypadRead.py
+++ keypadRead.py
@@ -9,14 +9,20 @@ controls = (14,15) #list of control wires
 GPIO.setup(controls, GPIO.OUT)
 
 def key2pi():
+    GPIO.setmode(GPIO.BCM)
     inputs = (9, 10, 11)
+    controls = (14,15) #list of control wires
+    GPIO.setup(controls, GPIO.OUT)
     GPIO.output(14, False) #disable LOAD
     GPIO.output(15, True) #enable OUTPUT-ENABLE
     time.sleep(0.01)
     GPIO.setup(inputs, GPIO.OUT)
 
 def pi2key():
+    GPIO.setmode(GPIO.BCM)
     inputs = (9, 10, 11)
+    controls = (14,15) #list of control wires
+    GPIO.setup(controls, GPIO.OUT)
     GPIO.output(14, True) #disable LOAD
     GPIO.output(15, False) #enable OUTPUT-ENABLE
     time.sleep(0.01)
@@ -74,7 +80,8 @@ except IOError:
     password = '1234'
 
 print(password)
-for i in range(len(password)):
+for i in range(len(password)-1):
     letter = str(keypadRead())
     if (password[i] != letter):
         print("Wrong password")
+        print("should be", password[i])
